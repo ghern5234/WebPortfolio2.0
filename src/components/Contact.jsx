d0faa7d887d9fc28f51e70451c1271d066bdc919
George <ghern5234@gmail.com>
2024-08-13 00:42:18 -0500

    imported and updated icons.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
-import emailImage from '../assets/email.png'
-import gitImage from '../assets/git.png'
+import emailImage from '../assets/mail.png'
+import gitImage from '../assets/github.png'
 import linkedInImage from '../assets/linkedin.png'
 import { Link } from "react-router-dom";
 
@@ -7,9 +7,9 @@ const Contact = () => {
     return (
         <>
         <div className='flex flex-row mt-20  transition'>
-            <a href="mailto:ghern5234@gmail.com" ><img src={emailImage} width={200} height={300} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110 '></img></a>
             <Link  to="https://github.com/ghern5234"><img src={gitImage} width={200} height={250} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110'></img></Link>
             <Link h><img src={linkedInImage} width={200} height={250} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110'></img></Link>
+            <a href="mailto:ghern5234@gmail.com" ><img src={emailImage} width={200} height={300} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110 '></img></a>
         </div>
 
 
