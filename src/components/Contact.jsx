6678051f9445c1436f9def54f6b6f068e8297bda
George <ghern5234@gmail.com>
2024-08-13 00:55:01 -0500

    updated linkedin link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <>
         <div className='flex flex-row mt-20  transition'>
             <Link  to="https://github.com/ghern5234"><img src={gitImage} width={200} height={250} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110'></img></Link>
-            <Link h><img src={linkedInImage} width={200} height={250} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110'></img></Link>
+            <Link to="https://www.linkedin.com/in/ghern5234/"><img src={linkedInImage} width={200} height={250} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110'></img></Link>
             <a href="mailto:ghern5234@gmail.com" ><img src={emailImage} width={200} height={300} className='m-2 z-0 rounded-lg transition-all duration-300 hover:scale-110 '></img></a>
         </div>
 
