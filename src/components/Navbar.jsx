ff89bbeb6c97cc93c257af745cce31281c9715f8
George <ghern5234@gmail.com>
2024-08-13 22:17:04 -0500

    updated font and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
     <ul className={`font-bold text-cyan w-screen bg-black flex flex-row items-center justify-center`} style={{
       height: `${NAVBAR_REM_HEIGHT}rem`
     }}>
-      {path!=="/"&&<li className="p-2 hover:bg-teal rounded-xl hover:text-white transition mr-auto"><a href={"/"}>Home</a></li>}
+      {path!=="/"&&<li className="p-5 hover:bg-teal rounded-xl hover:text-white transition mr-auto text-2xl"><a href={"/"}>Home</a></li>}
       {Links.map((item, index)=> {
       const {link, name} = item
       
       return (
-        <li className="p-2 hover:bg-teal rounded-xl hover:text-white transition mx-2" key={index}><a href={link}>{name}</a></li>
+        <li className="p-5 hover:bg-teal rounded-xl hover:text-white transition mx-2 text-2xl" key={index}><a href={link}>{name}</a></li>
 
       )
     })} 
-     {path!=="/"&&<li className="p-2 rounded-xl  transition ml-auto text-transparent">Home</li>}
+     {path!=="/"&&<li className="p-4 rounded-xl  transition ml-auto text-transparent">Home</li>}
     </ul>
   )
 }
--- src/utils/globals.jsx
+++ src/utils/globals.jsx
@@ -3,7 +3,7 @@ import Contact from "../components/Contact";
 import Projects from "../components/Projects";
 import Resume from "../components/Resume";
 
-export const NAVBAR_REM_HEIGHT = 4;
+export const NAVBAR_REM_HEIGHT = 6;
 export const Links = [{
     link: "/About",
     name: "About",
